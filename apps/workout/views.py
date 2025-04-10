6ca29b14ddb241603635ea2f5e34b65d058fc8d8
Your Name <you@example.com>
2025-04-10 14:50:33 +0600

    first commit

---
+++ apps/workout/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets, permissions
+from .models import Workout
+from .serializers import WorkoutSerializer
+
+class WorkoutViewSet(viewsets.ModelViewSet):
+    serializer_class = WorkoutSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Workout.objects.filter(user=self.request.user)
